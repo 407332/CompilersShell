a2f8e5c9baecaa425d9019ff9b103167720d0f7c
Roy <royklaassen7333@gmail.com>
2018-03-12 21:58:33 +0100

    cd ls en pwd klaar, no error handling

--- src/SimpleCommand.cpp
+++ src/SimpleCommand.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include "SimpleCommand.h"
 #include <unistd.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <cstring>
 
 using namespace std;
 
@@ -9,22 +12,52 @@ void SimpleCommand::execute() {
     // FIXME: Your code here...
     char cwd[1024];
 
+
     if (command == "pwd") {
 
+        getcwd(cwd, sizeof(cwd));
 
-        if (getcwd(cwd, sizeof(cwd)) != nullptr) {
+        if (cwd != nullptr) {
             cout << "Current working dir:" << cwd;
         } else {
             cerr << "Error getting current working directory";
         }
 
-
     } else if (command == "ls") {
-        const char *homePath = std::getenv("HOME");
 
-        std::cout << homePath << " ";
-    } else if (command == "cd"){
-        
+        DIR *dir = opendir(".");
+        struct dirent *entry = readdir(dir);
+        while (entry != nullptr) {
+            cout << entry->d_name << "\n";
+            entry = readdir(dir);
+        }
+        closedir(dir);
+
+    } else if (command == "cd") {
+        if(arguments.empty()){
+            strcpy(cwd, getenv("HOME"));
+        }else{
+            getcwd(cwd, sizeof(cwd));
+
+            if(arguments[0] == ".."){
+
+                for(int i =1024; i>0; i--){
+                    if(cwd[i]=='/'){
+                        cwd[i] = '\0';
+                        i=0;
+                    }
+                }
+
+            }else {
+                strcat(cwd, "/");
+                strcat(cwd,arguments[0].c_str());
+
+            }
+
+        }
+
+        cout << cwd;
+        chdir(cwd);
     }
 
     cout << "\n";
