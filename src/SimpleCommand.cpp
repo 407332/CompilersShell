5770b6ea5993ba8c42ba1e3c65e8906013c9ce3b
Roy <royklaassen7333@gmail.com>
2018-03-12 20:31:38 +0100

    fixed

--- src/SimpleCommand.cpp
+++ src/SimpleCommand.cpp
@@ -1,19 +1,11 @@
 #include <iostream>
 #include "SimpleCommand.h"
-#include "main.cpp"
 
-using namespace std;
 void SimpleCommand::execute() {
-	//cout << "FIXME: You should change SimpleCommand::execute()" << std::endl;
+	std::cout << "FIXME: You should change SimpleCommand::execute()" << std::endl;
 	// FIXME: Your code here...
 
 
 
-	if(command == "cd"){
-        const char* homePath = std::getenv("HOME");
-
-        cout << homePath << " ";
-	}else if(command == "ls"){
-
-    }
+    
 }
