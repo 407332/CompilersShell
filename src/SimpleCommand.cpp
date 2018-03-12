8e55b4ded6d061d9ef30e342235f376f56f7cf01
Roy <royklaassen7333@gmail.com>
2018-03-12 20:52:24 +0100

    push

--- src/SimpleCommand.cpp
+++ src/SimpleCommand.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
 #include "SimpleCommand.h"
+#include <unistd.h>
+
+using namespace std;
 
 void SimpleCommand::execute() {
-	std::cout << "FIXME: You should change SimpleCommand::execute()" << std::endl;
-	// FIXME: Your code here...
+    //cout << "FIXME: You should change SimpleCommand::execute()" << std::endl;
+    // FIXME: Your code here...
+    char cwd[1024];
+
+    if (command == "pwd") {
 
 
+        if (getcwd(cwd, sizeof(cwd)) != nullptr) {
+            cout << "Current working dir:" << cwd;
+        } else {
+            cerr << "Error getting current working directory";
+        }
 
-    
+
+    } else if (command == "ls") {
+        const char *homePath = std::getenv("HOME");
+
+        std::cout << homePath << " ";
+    } else if (command == "cd"){
+        
+    }
+
+    cout << "\n";
 }
